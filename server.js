fd8a2e3c6bc7f359e37cedfad63d0a6384b383a8
Jeremy Robles <jeremy.robles56@gmail.com>
2016-07-08 00:16:43 -0700

    updated directional controls and tests

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 var Cylon = require('cylon');
+var http = require('http');
+var curl = require('curlrequest');
+var express = require("express"),
+    app = express(),
+    bodyParser = require('body-parser'),
+    errorHandler = require('errorhandler'),
+    methodOverride = require('method-override'),
+    hostname = process.env.HOSTNAME || 'localhost',
+    port = parseInt(process.env.PORT, 10) || 8080,
+    publicDir = process.argv[2] || __dirname + '/public',
+    path = require('path');
+
+
+
+
+
 //after the server is started use ngrok to tunnel the api
 Cylon.api({
     host: '127.0.0.1',
-    port: '8080',
+    port: '4040',
     ssl: false,
     auth:false
 });
 var test = Cylon.robot({
     name: 'omnius',
     connections: {
-        arduino: {adaptor: 'firmata', port: '/dev/cu.usbmodem1421'}
+        arduino: {adaptor: 'firmata', port: '/dev/cu.usbmodem34'}
     },
     // devices: {
     //     motor1PWR: {driver: 'direct-pin', pin: 7},
@@ -39,7 +55,7 @@ var test = Cylon.robot({
             test.connections.arduino.digitalWrite(5, 0);
             test.connections.arduino.digitalWrite(4, 1);
         });
-        after((5).seconds(), function() {
+        after((2).seconds(), function() {
             test.connections.arduino.digitalWrite(7, 0);
             test.connections.arduino.digitalWrite(6, 0);
             test.connections.arduino.digitalWrite(5, 0);
@@ -58,7 +74,7 @@ var test = Cylon.robot({
             test.connections.arduino.digitalWrite(5, 1);
             test.connections.arduino.digitalWrite(4, 0);
         });
-        after((5).seconds(), function() {
+        after((2).seconds(), function() {
             test.connections.arduino.digitalWrite(7, 0);
             test.connections.arduino.digitalWrite(6, 0);
             test.connections.arduino.digitalWrite(5, 0);
@@ -77,7 +93,7 @@ var test = Cylon.robot({
             test.connections.arduino.digitalWrite(5, 1);
             test.connections.arduino.digitalWrite(4, 0);
         });
-        after((5).seconds(), function() {
+        after((2).seconds(), function() {
             test.connections.arduino.digitalWrite(7, 0);
             test.connections.arduino.digitalWrite(6, 0);
             test.connections.arduino.digitalWrite(5, 0);
@@ -96,7 +112,7 @@ var test = Cylon.robot({
             test.connections.arduino.digitalWrite(5, 0);
             test.connections.arduino.digitalWrite(4, 1);
         });
-        after((5).seconds(), function() {
+        after((2).seconds(), function() {
             test.connections.arduino.digitalWrite(7, 0);
             test.connections.arduino.digitalWrite(6, 0);
             test.connections.arduino.digitalWrite(5, 0);
@@ -106,3 +122,43 @@ var test = Cylon.robot({
     }
 });
 test.start();
+
+
+app.get("/api/robots/omnius/commands/forward", function (req, res) {
+    res.sendStatus(200);
+    curl.request('http://localhost:4040/api/robots/omnius/commands/forward', function(err) {
+
+    });
+});
+app.get("/api/robots/omnius/commands/backward", function (req, res) {
+    res.sendStatus(200);
+    curl.request('http://localhost:4040/api/robots/omnius/commands/backward', function(err) {
+
+    });
+});
+app.get("/api/robots/omnius/commands/left", function (req, res) {
+    res.sendStatus(200);
+    curl.request('http://localhost:4040/api/robots/omnius/commands/left', function(err) {
+
+    });
+});
+app.get("/api/robots/omnius/commands/right", function (req, res) {
+    res.sendStatus(200);
+    curl.request('http://localhost:4040/api/robots/omnius/commands/right', function(err) {
+
+    });
+});
+
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static(publicDir));
+app.use(errorHandler({
+    dumpExceptions: true,
+    showStack: true
+}));
+
+console.log("Simple static server showing %s listening at http://%s:%s", publicDir, hostname, port);
+app.listen(port, hostname);
--- test.js
+++ test.js
@@ -1,19 +1,15 @@
-var five = require("johnny-five"),
-    api = require("cylon-api-http"),
-    board = new five.Board({
-        // repl: false,
-        debug: false,
-        port: "/dev/cu.usbmodem1421"
-    });
-board.on("ready", function() {
-    this.repl.inject({
-        ledOn: function ledOn(){
-            board.pinMode(13, five.Pin.OUTPUT);
-            board.digitalWrite(13, 1);
-        },
-        ledOff: function ledOff(){
-            board.pinMode(13, five.Pin.OUTPUT);
-            board.digitalWrite(13, 0);
-        }
-    });
-});
+var request = require('request');
+
+// quest('https://7bce550f.ngrok.io/api/robots/omnius/commands/forward', function () {
+//     if (this.statusCode == 200) {
+//         console.log(this);
+//     }else{
+//         console.log('failed');
+//     }
+// });
+// var request = require('request');
+request('https://7bce550f.ngrok.io/api/robots/omnius/commands/forward', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body); // Show the HTML for the Google homepage.
+    }
+})
